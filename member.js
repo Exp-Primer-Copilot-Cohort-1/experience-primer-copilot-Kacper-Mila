9508cd23578f316bd06b5af5747d6562911cac17
Kacper Mila <77899626+Kacper-Mila@users.noreply.github.com>
2023-12-10 10:14:41 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,8 @@
-function skillMember() {
-    var skill = document.getElementById("skill").value;
-    var member = document.getElementById("member").value;
-    var skillMember = document.getElementById("skillMember");
-    var skillMemberList = document.getElementById("skillMemberList");
-    var skillMemberListLi = document.createElement("li");
-    var skillMemberListLiText = document.createTextNode(skill + " : " + member);
-    skillMemberListLi.appendChild(skillMemberListLiText);
-    skillMemberList.appendChild(skillMemberListLi);
-    skillMember.value = "";
-    member.value = "";
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/member/skills.html',
+    controller: 'MemberController',
+    controllerAs: 'memberCtrl'
+  };
 }
